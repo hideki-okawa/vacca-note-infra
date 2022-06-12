fce712af83e3d21a0d566d70fa36642dd7a1b67d
Okaki030 <h.okawa0303@gmail.com>
2022-06-12 11:05:54 +0900

    Reactアプリ用のS3バケットとCloudfrontディストリビューションを作成

---
+++ view/main/cloud_front.tf
@@ -0,0 +1,49 @@
+resource "aws_cloudfront_distribution" "this" {
+  origin {
+    domain_name = aws_s3_bucket.this.bucket_regional_domain_name
+    origin_id   = aws_s3_bucket.this.id
+
+    s3_origin_config {
+      origin_access_identity = aws_cloudfront_origin_access_identity.static-www.cloudfront_access_identity_path
+    }
+  }
+
+  enabled             = true
+  default_root_object = "index.html"
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = aws_s3_bucket.this.id
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "allow-all"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "whitelist"
+      locations        = ["JP"]
+    }
+  }
+
+  tags = {
+    Name = local.system_name
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+}
+
+resource "aws_cloudfront_origin_access_identity" "static-www" {}
---
+++ view/main/s3.tf
@@ -0,0 +1,30 @@
+resource "aws_s3_bucket" "this" {
+  bucket = "${local.system_name}-view"
+
+  tags = {
+    Name = "${local.system_name}-view"
+  }
+}
+
+resource "aws_s3_bucket_policy" "this" {
+    bucket = aws_s3_bucket.this.id
+    policy = data.aws_iam_policy_document.static-www.json
+}
+
+data "aws_iam_policy_document" "static-www" {
+  statement {
+    sid = "Allow CloudFront"
+    effect = "Allow"
+    principals {
+        type = "AWS"
+        identifiers = [aws_cloudfront_origin_access_identity.static-www.iam_arn]
+    }
+    actions = [
+        "s3:GetObject"
+    ]
+
+    resources = [
+        "${aws_s3_bucket.this.arn}/*"
+    ]
+  }
+}

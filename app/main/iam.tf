ead5a73dbb082a95e99e196cdde9446cef829ab5
Okaki030 <h.okawa0303@gmail.com>
2022-06-16 08:37:58 +0900

    ECSタスクでパラメータストアを利用するための権限設定

--- app/main/iam.tf
+++ app/main/iam.tf
@@ -30,6 +30,36 @@ resource "aws_iam_role_policy_attachment" "ecs_task_execution" {
   policy_arn = data.aws_iam_policy.ecs_task_execution.arn
 }
 
+resource "aws_iam_policy" "ssm" {
+  name = "${local.system_name}-ssm"
+
+  # パラメータストアの値読み取り権限
+  policy = jsonencode(
+    {
+      "Version" : "2012-10-17",
+      "Statement" : [
+        {
+          "Effect" : "Allow",
+          "Action" : [
+            "ssm:GetParameters",
+            "ssm:GetParameter"
+          ],
+          "Resource" : "arn:aws:ssm:${data.aws_region.current.id}:${data.aws_caller_identity.self.account_id}:parameter/${local.system_name}/prod/*"
+        }
+      ]
+    }
+  )
+
+  tags = {
+    Name = "${local.system_name}-ssm"
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_ssm" {
+  role       = aws_iam_role.ecs_task_execution.name
+  policy_arn = aws_iam_policy.ssm.arn
+}
+
 resource "aws_iam_role" "ecs_task" {
   name = "${local.system_name}-ecs-task"
 
